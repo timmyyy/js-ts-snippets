b918424ea09750a44c328b3b493488aea33ccc77
Насибуллин Тимур Зуфарович <nasibullin.t.zu@sberbank.ru>
2019-11-26 16:27:02 +0300

    lodash forown async await

---
+++ index.ts
@@ -0,0 +1,32 @@
+import _ = require("lodash")
+
+const map = {
+    asd1: "qwe1",
+    asd2: "qwe2",
+    asd3: "qwe3",
+    asd4: "qwe4",
+    asd5: "qwe5",
+    asd6: "qwe6",
+    asd7: "qwe7",
+    asd8: "qwe8",
+    asd9: "qwe9",
+    asd: "qwe"
+}
+
+async function myawait( asd, qwe ) {
+    setTimeout(()=>{console.log ()}, 100)
+    console.log( `processing in async await: ${ asd } - ${ qwe }` )
+    console.log(`returning asd ${asd}`)
+    return asd
+}
+
+_( map ).forOwn( async ( asd, qwe ) => {
+    console.log("start")
+    const asdRet = await myawait( asd, qwe )
+    //everything after await is processed after all iterations
+    console.log(`asdRet ${asdRet}`)
+    console.log("finish")
+} )
+
+console.log("\n---------------------\n")
+
